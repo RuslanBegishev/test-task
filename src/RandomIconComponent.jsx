f49bf5018b510f33710f90b127e479e7a1f743dc
RuslanBegishev <ruslanbegishev93@gmail.com>
2023-10-07 11:11:09 +0300

    first

---
+++ src/RandomIconComponent.jsx
@@ -0,0 +1,35 @@
+﻿import React, { useState } from "react";
+import './RandomIconComponent.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCar, faBicycle, faBus, faTrain, faRocket, faShip, faPlane, faMotorcycle } from "@fortawesome/free-solid-svg-icons";
+
+const icons = [faCar, faBicycle, faBus, faTrain, faRocket, faShip, faPlane, faMotorcycle];
+
+function RandomIconComponent() {
+  const [randomIcon, setRandomIcon] = useState(null);
+
+  const showRandomIcon = () => {
+    const randomIndex = Math.floor(Math.random() * icons.length);
+    const selectedIcon = icons[randomIndex]
+    setRandomIcon(selectedIcon);
+ 
+    setTimeout(() => {
+      setRandomIcon(null);
+    }, 3000);   
+  };
+
+  return (
+    <div className="container">
+      <div className="button-container">
+        <button className="button" onClick={showRandomIcon}>
+          Show random icon
+        </button>
+      </div>
+      <div>
+        {randomIcon && <FontAwesomeIcon icon={randomIcon} size='3x' />}
+      </div>
+    </div>
+  );
+}
+
+export default RandomIconComponent;
